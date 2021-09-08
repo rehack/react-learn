6b2254e04e09b582416a3cb26a36048960df47e7
rehack <565195693@qq.com>
2021-09-09 01:05:18 +0800

    update

--- src/redux/reducers/person.js
+++ src/redux/reducers/person.js
@@ -9,7 +9,11 @@ const personReducer = (prevState=initState,action) => {
     const {type,data} = action
     switch (type) {
         case ADD_PERSON:
-            return [data,...prevState] // 添加一条数据
+            /**
+             * 不能使用prevState.unshift(data)这种方式来添加，push和unshift添加后prevState数组的引用地址并未改变
+             * 同时也破坏了reducer为纯函数(参数不能被改写)的原则
+             */
+            return [data,...prevState] // 添加一条数据，返回的是一个新数组
         default:
             return prevState
     }
